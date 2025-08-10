6cc65396eac9960d406b86d7e9a1a66037a9a6bc
qqfunc <qqfunc@gmail.com>
2025-08-10 22:21:39 +0900

    Update and rename Symbol.swift to SFSymbol.swift

--- Sources/SFSymbols/SFSymbol.swift
+++ Sources/SFSymbols/SFSymbol.swift
@@ -1,6 +1,6 @@
 // This file is generated automatically. Do not edit.
 
-public enum Symbol: String {
+public enum SFSymbol: String {
 
     @available(iOS 13.0, macOS 10.15, tvOS 13.0, visionOS 1.0, watchOS 6.0, *)
     case `0.circle` =  "0.circle"
@@ -24547,4 +24547,4 @@ public enum Symbol: String {
 
 }
 
-extension Symbol: CaseIterable {}
+extension SFSymbol: CaseIterable {}
