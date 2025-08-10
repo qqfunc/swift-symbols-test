9f9f9d7dbe71061bcf21584a39f80e401a8f684b
qqfunc <qqfunc@gmail.com>
2025-08-10 22:54:34 +0900

    Update Image+Symbol.swift

--- Sources/SFSymbols/Image+Symbol.swift
+++ Sources/SFSymbols/Image+Symbol.swift
@@ -8,7 +8,7 @@ extension Image {
     /// Initializes an `Image` with a system symbol.
     /// - Parameter systemSymbol: The symbol to use for the image.
     @available(iOS 13.0, macCatalyst 13.0, macOS 11.0, tvOS 13.0, visionOS 1.0, watchOS 6.0, *)
-    public convenience init(systemSymbol: SFSymbol) {
+    public init(systemSymbol: SFSymbol) {
         self.init(systemName: systemSymbol.rawValue)
             ?? { fatalError("Cannot initialize Image with symbol: \(systemSymbol.rawValue)") }()
     }
