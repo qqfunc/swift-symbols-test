50ad4016b2ef62f530c075c39d492f332f29ef00
qqfunc <qqfunc@gmail.com>
2025-08-11 07:30:28 +0900

    Update UIImage+Symbol.swift

--- Sources/SFSymbols/UIImage+Symbol.swift
+++ Sources/SFSymbols/UIImage+Symbol.swift
@@ -9,8 +9,7 @@ extension UIImage {
     /// - Parameter systemSymbol: The symbol to use for the image.
     @available(iOS 13.0, macCatalyst 13.1, tvOS 13.0, visionOS 1.0, watchOS 6.0, *)
     public convenience init(systemSymbol: SFSymbol) {
-        self.init(systemName: systemSymbol.rawValue)
-            ?? { fatalError("Cannot initialize UIImage with symbol: \(systemSymbol.rawValue)") }()
+        self.init(systemName: systemSymbol.rawValue)!
     }
 
 }
