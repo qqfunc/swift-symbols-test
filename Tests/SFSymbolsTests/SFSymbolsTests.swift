0fcbe8b175c249a3e903dc02c15f543586139d9e
qqfunc <qqfunc@gmail.com>
2025-08-11 18:22:30 +0900

    Update SFSymbolsTests.swift

--- Tests/SFSymbolsTests/SFSymbolsTests.swift
+++ Tests/SFSymbolsTests/SFSymbolsTests.swift
@@ -2,6 +2,6 @@ import Testing
 @testable import SFSymbols
 
 @Test func example() async throws {
-    SFSymbol.`arrow.backward`
+    SFSymbol.`0.circle`
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
 }
