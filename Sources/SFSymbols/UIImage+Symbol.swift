23c113c050e61e6c3726d6ed467f73a30367ac25
qqfunc <qqfunc@gmail.com>
2025-09-17 19:55:11 +0900

    Update

--- Sources/SFSymbols/UIImage+Symbol.swift
+++ Sources/SFSymbols/UIImage+Symbol.swift
@@ -5,12 +5,14 @@ import UIKit
 @available(iOS 2.0, macCatalyst 13.1, visionOS 1.0, watchOS 2.0, *)
 extension UIImage {
 
+    /*
     /// Initializes a `UIImage` with a system symbol.
     /// - Parameter systemSymbol: The symbol to use for the image.
     @available(iOS 13.0, macCatalyst 13.1, tvOS 13.0, visionOS 1.0, watchOS 6.0, *)
     public convenience init(systemSymbol: SFSymbol) {
         self.init(systemName: systemSymbol.rawValue)!
     }
+    */
 
 }
 
