5adbd1646c61390c56d95e12d52bf48ceadb87d7
qqfunc <qqfunc@gmail.com>
2025-09-18 19:28:07 +0900

    Update SFSymbolsTests.swift

--- Tests/SFSymbolsTests/SFSymbolsTests.swift
+++ Tests/SFSymbolsTests/SFSymbolsTests.swift
@@ -1,7 +1,7 @@
 import Testing
 @testable import SFSymbols
 
-@Test func example() async throws {
-    SFSymbol.`0.circle`
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Test func testSymbolNames() async throws {
+    #expect(SFSymbol.circle.name == "circle")
+    #expect(SFSymbol.`square.fill`.name == "square.fill")
 }
